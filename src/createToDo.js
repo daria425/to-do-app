24e8ce5acdb6fc483fd061e55e86c04b28982115
daria425 <darianaumova5@gmail.com>
2023-07-26 15:32:37 +0300

    Add functionality to store toDos and projects

--- src/createToDo.js
+++ src/createToDo.js
@@ -1,5 +1,9 @@
 class project {
   toDoItemsArray = [];
+  constructor(name, description) {
+    this.name = name;
+    this.description = description;
+  }
 }
 class toDo {
   constructor(title, description, dueDate, priority) {
@@ -10,29 +14,68 @@ class toDo {
   }
 }
 
-const defaultProject = new project();
+//storage of all project objects
+const projectsArray = [];
+
+//default project for storing toDos
+const defaultProject = new project("New Project");
+projectsArray.push(defaultProject);
 
 function createToDo() {
   const titleInput = document.querySelector("input[name=title]");
   const descInput = document.querySelector("input[name=description]");
   const dateInput = document.querySelector("input[name=due-date]");
   const priorityInput = document.querySelector("select");
-  const form = document.querySelector("form");
-  const submitBtn = document.querySelector(".submit-todo");
 
-  form.addEventListener("submit", function (e) {
-    e.preventDefault();
-    const testItem = new toDo(
-      titleInput.value,
-      descInput.value,
-      dateInput.value,
-      priorityInput.value
-    );
-    defaultProject.toDoItemsArray.push(testItem);
-    console.log(defaultProject);
-  });
-
-  return defaultProject;
+  const newItem = new toDo(
+    titleInput.value,
+    descInput.value,
+    dateInput.value,
+    priorityInput.value
+  );
+  console.log(newItem);
+  return newItem;
 }
 
-export { createToDo };
+function createProject() {
+  const projectTitleInput = document.querySelector("input[name=project-title]");
+  const projectDescInput = document.querySelector(
+    "input[name=project-description]"
+  );
+
+  const newProject = new project(
+    projectTitleInput.value,
+    projectDescInput.value
+  );
+  console.log(newProject);
+  return newProject;
+}
+
+//event listeners
+function submitToDo() {
+  const toDoForm = document.querySelector(".to-do-form");
+  toDoForm.addEventListener("submit", addToProject);
+}
+
+function submitProject() {
+  const projectForm = document.querySelector(".project-form");
+  projectForm.addEventListener("submit", addNewProject);
+}
+
+//on to-do form submit, create a toDo and push it to the project
+function addToProject(e) {
+  e.preventDefault();
+  const item = createToDo();
+  defaultProject.toDoItemsArray.push(item);
+  console.log(defaultProject);
+}
+
+//on project form submit, make a new project objecg and push to the array of projects
+function addNewProject(e) {
+  e.preventDefault();
+  const userProject = createProject();
+  projectsArray.push(userProject);
+  console.log(projectsArray);
+}
+
+export { createToDo, submitToDo, submitProject, addToProject };
